2924aaa7b31b408b9f97b88a4d307152ecb1e34d
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-05 16:21:04 +0200

    refactor: revise ExpireableValue

--- src/server/entity/ai/memory/ExpireableValue.lua
+++ src/server/entity/ai/memory/ExpireableValue.lua
@@ -8,23 +8,35 @@ export type ExpireableValue<T> = typeof(setmetatable({} :: {
 	timeToLive: number
 }, ExpireableValue))
 
-function ExpireableValue.new<T>(value: T, expiresIn: number): ExpireableValue<T>
+function ExpireableValue.new<T>(value: T, timeToLive: number): ExpireableValue<T>
 	return setmetatable({
 		value = value,
-		timeToLive = expiresIn,
+		timeToLive = timeToLive,
 	}, ExpireableValue)
+end
+
+function ExpireableValue.nonExpiring<T>(value: T): ExpireableValue<T>
+	return ExpireableValue.new(value, math.huge)
 end
 
 function ExpireableValue.getValue<T>(self: ExpireableValue<T>): T?
 	return self.value
 end
 
+function ExpireableValue.getTimeToLive<T>(self: ExpireableValue<T>): number
+	return self.timeToLive
+end
+
+function ExpireableValue.canExpire<T>(self: ExpireableValue<T>): boolean
+	return self.timeToLive ~= math.huge
+end
+
 function ExpireableValue.isExpired<T>(self: ExpireableValue<T>): boolean
-	return self.timeToLive < 0
+	return self.timeToLive <= 0
 end
 
 function ExpireableValue.update<T>(self: ExpireableValue<T>, delta: number): ()
-	if self.timeToLive > 0 then
+	if self:canExpire() then
 		self.timeToLive -= delta
 	end
 end
